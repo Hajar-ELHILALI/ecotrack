778c53fa72377163d14724cbd9d3e44e38d87fda
Hajar-ELHILALI <hajarelhilali57@gmail.com>
2025-07-02 15:22:48 +0100

    updating Country.java

--- server/src/main/java/EcoTrack/server/entity/Country.java
+++ server/src/main/java/EcoTrack/server/entity/Country.java
@@ -1,16 +1,24 @@
 package EcoTrack.server.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "Countries")
 public class Country {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String name;
     private String code_iso;
 
