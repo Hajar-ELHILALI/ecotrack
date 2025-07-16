d56a9801bfe7be7eab5c97f5739142e9c3acd220
Yasminekoulam <yasminekoulam2004@gmail.com>
2025-07-17 00:43:36 +0100

    adding files of config

--- server/src/main/java/EcoTrack/server/repository/UserRepository.java
+++ server/src/main/java/EcoTrack/server/repository/UserRepository.java
@@ -3,7 +3,12 @@ package EcoTrack.server.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import EcoTrack.server.entity.User;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    void deleteByEmail(String email);
+    User getByEmail(String email);
+
 }
---
+++ server/src/main/java/EcoTrack/server/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package EcoTrack.server.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import EcoTrack.server.entity.User;
+import EcoTrack.server.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+        if (optionalUser.isPresent()) {
+            return new UserDetailsImpl(optionalUser.get());
+        } else {
+            throw new UsernameNotFoundException(email);
+        }
+    }
+}
