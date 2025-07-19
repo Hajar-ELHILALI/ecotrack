7f16880b3a5720585851df3296ec92617c81b157
Yasminekoulam <yasminekoulam2004@gmail.com>
2025-07-19 21:00:40 +0100

    zedt msse7t my mistakes:)

--- server/src/main/java/EcoTrack/server/service/implementation/UserServiceImpl.java
+++ server/src/main/java/EcoTrack/server/service/implementation/UserServiceImpl.java
@@ -1,48 +1,23 @@
 package EcoTrack.server.service.implementation;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import EcoTrack.server.DTO.RegisterRequestDTO;
 import EcoTrack.server.DTO.UserDTO;
 import EcoTrack.server.entity.User;
 import EcoTrack.server.repository.UserRepository;
 import EcoTrack.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import EcoTrack.server.exception.NotFoundException;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
-
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final UserDetailsService userDetailsService;
-
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) {
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.userDetailsService = userDetailsService;
-
-    }
-
-    @Override
-    public ResponseEntity<UserDTO> register(RegisterRequestDTO registerRequestDTO) {
-        User user = registerRequestDTO.user();
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        //user.setRole();
-        userRepository.save(user);
-        return ResponseEntity.ok(new UserDTO(user));
     }
 
     @Override
