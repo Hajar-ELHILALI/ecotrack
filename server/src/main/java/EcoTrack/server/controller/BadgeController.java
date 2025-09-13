651a2f4d7c16c533f95e46fb16ad9cc16016d4a2
Yasminekoulam <yasminekoulam2004@gmail.com>
2025-09-13 04:01:20 +0200

    add endpoint for logged-in user to view their badge

--- server/src/main/java/EcoTrack/server/controller/BadgeController.java
+++ server/src/main/java/EcoTrack/server/controller/BadgeController.java
@@ -2,18 +2,24 @@ package EcoTrack.server.controller;
 
 import EcoTrack.server.DTO.BadgeDTO;
 
+import EcoTrack.server.entity.User;
+import EcoTrack.server.repository.UserRepository;
 import EcoTrack.server.service.BadgeService;
+import EcoTrack.server.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/badges")
 public class BadgeController {
     private final BadgeService badgeService;
-    public BadgeController(BadgeService badgeService) {
+    private final UserRepository userRepository;
+    public BadgeController(BadgeService badgeService,UserRepository userRepository) {
         this.badgeService = badgeService;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/")
@@ -38,5 +44,18 @@ public class BadgeController {
     @DeleteMapping("/{id}")
     public void deleteBadge(@PathVariable Long id) {
         badgeService.deleteDTOById(id);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<String> getMyBadgeLabel(Principal principal) {
+        String email = principal.getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (user.getBadge() == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(user.getBadge().getLabel().name());
     }
 }
