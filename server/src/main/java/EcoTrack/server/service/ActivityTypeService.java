639711bd9edc46baeb40651bdfac68ad3174d8e6
Hajar-ELHILALI <hajarelhilali57@gmail.com>
2025-09-17 20:24:06 +0100

    add findActivityTypesByCategory

--- server/src/main/java/EcoTrack/server/service/ActivityTypeService.java
+++ server/src/main/java/EcoTrack/server/service/ActivityTypeService.java
@@ -2,6 +2,10 @@ package EcoTrack.server.service;
 
 import EcoTrack.server.DTO.ActivityTypeDTO;
 import EcoTrack.server.entity.ActivityType;
+import EcoTrack.server.entity.Category;
+
+import java.util.List;
 
 public interface ActivityTypeService extends CrudDTO<ActivityTypeDTO, Long> {
+    List<ActivityTypeDTO> findActivityTypesByCategory(Long categoryId);
 }
