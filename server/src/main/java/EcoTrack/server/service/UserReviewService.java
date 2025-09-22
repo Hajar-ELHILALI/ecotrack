08077db700d38e5712abec84b6348b99a1ecdd27
Hajar-ELHILALI <hajarelhilali57@gmail.com>
2025-09-22 01:27:12 +0100

    fixing and adding some code

--- server/src/main/java/EcoTrack/server/service/UserReviewService.java
+++ server/src/main/java/EcoTrack/server/service/UserReviewService.java
@@ -3,5 +3,8 @@ package EcoTrack.server.service;
 import EcoTrack.server.DTO.UserReviewDTO;
 import EcoTrack.server.entity.UserReview;
 
+import java.util.List;
+
 public interface UserReviewService extends CrudDTO<UserReviewDTO, Long>{
+    List<UserReview> findUserReviews(Long id);
 }
