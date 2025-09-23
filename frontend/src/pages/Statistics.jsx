b81aeca0dd2e91f97b684eb8c3bd5c3c5c1f161f
Hajar-ELHILALI <hajarelhilali57@gmail.com>
2025-09-23 01:03:56 +0100

    add advices page

--- frontend/src/pages/Statistics.jsx
+++ frontend/src/pages/Statistics.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, BarChart, Bar, PieChart, Pie, Cell } from 'recharts';
+
 import axios from 'axios';
 import AccountNavbar from '../components/AccountNavbar';
 
