51d3fbb4845069a562fe3d0f7f3458ca45d1951e
Hajar ELHILALI <hajarelhilali57@gmail.com>
2025-10-30 21:38:13 +0100

    update

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,17 +3,18 @@ import { Link } from "react-router-dom";
 
 const Card = ({ image, title,href }) => {
   return (
-    <div className="flex flex-col items-center bg-white shadow-md rounded-xl p-4 hover:shadow-lg transition duration-300 h-64">
+    <div className="flex flex-col items-center bg-white shadow-md rounded-xl p-4 hover:shadow-lg transition duration-300 h-48 w-48">
       <Link to={href}>
       <img 
         src={image} 
         alt={title} 
-        className="h-40 w-40 object-cover rounded-lg"
+        className="h-32 w-32 object-cover rounded-lg"
       />
-      </Link>
-      <p className="mt-2 text-lg font-bold text-orange-500" >
+      <p className="mt-2 text-lg font-bold text-orange-500 text-center" >
         {title}
       </p>
+      </Link>
+      
     </div>
   )
 }
